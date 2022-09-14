846ee5d4e9b76eb187f85304cd9a1f63655babae
Mouiad Ali <mouiad.alilo@gmail.com>
2022-09-14 11:06:08 +0000

    Finish custom template tags

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ import blog.views
 from django.urls import path
 urlpatterns = [
     # other patterns
-    path("", blog.views.index)
+    path("", blog.views.index, name="blogs"),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
